1ad93d1c464e27c671c348a038ea2f0ecb483e61
Prashant Maurya <62278497+prashant94157@users.noreply.github.com>
2022-09-05 23:12:33 +0530

    Time: 4 ms (34.06%), Space: 5.9 MB (46.11%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,35 +1,16 @@
 class Solution {
-    double fun(double x,long long n)
-    {
-        if(x==0)
-            return 0;
-        if(x==1)
-            return 1;
-        if(n==-1)
-            return 1/x;
-        if(n==1)
-            return x;
-        if(n==0)
-            return 1;
-        // cout<<"vghcghh";
-        if(n>0)
-        {
-            double res = fun(x,n/2);
-            if(n%2)
-            {
-                return res*res*x;
-            }
-            return res*res;
-        }
-        double res=fun(x,n/2);
-        if((-n)%2)
-        {
-            return res*res/x;
-        }
-        return res*res;
-    }
+   
 public:
     double myPow(double x, int n) {
-        return fun(x,(long long)n);
+        return pow(x,(long long)n);
+    }
+    double pow(double x, long long n) {
+        if(n == 0)
+            return 1;
+        if(n<0){
+            n = -n;
+            x = 1/x;
+        }
+        return (n%2 == 0) ? pow(x*x, n/2) : x*pow(x*x, n/2);
     }
 };
