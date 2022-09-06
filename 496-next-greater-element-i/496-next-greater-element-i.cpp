8ce2f2d490abe2aacd995bc842384c9223bc74f8
Prashant Maurya <62278497+prashant94157@users.noreply.github.com>
2022-09-06 11:13:56 +0530

    Time: 13 ms (39.25%), Space: 9.1 MB (14.39%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums2.size();
+        map<int,int> ngtr;
+        stack<int> s;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s.size()==0)
+                ngtr[nums2[i]] = -1;
+            else if(s.top() > nums2[i])
+            {
+                ngtr[nums2[i]] = s.top();
+            }
+            else
+            {
+                while(s.size() && s.top()<=nums2[i])
+                    s.pop();
+                if(s.size()==0)
+                    ngtr[nums2[i]] = -1;
+                else
+                    ngtr[nums2[i]] = s.top();
+            }
+            s.push(nums2[i]);
+        }
+        vector<int> res(nums1.size());
+        int i=0;
+        for(auto a:nums1)
+            res[i++] = ngtr[a];
+        return res;
+    }
+};
