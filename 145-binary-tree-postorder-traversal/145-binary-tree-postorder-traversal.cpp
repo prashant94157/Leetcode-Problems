8e657c1256d14f07d0466593a4a07abd379c9cbf
Prashant Maurya <62278497+prashant94157@users.noreply.github.com>
2022-09-09 23:30:47 +0530

    Time: 8 ms (9.48%), Space: 8.4 MB (87.10%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -16,12 +16,35 @@ public:
         postorder(root,res);
         return res;
     }
+    // void postorder(TreeNode *root,vector<int> &res)
+    // {
+    //     if(root==NULL)
+    //         return;
+    //     postorder(root->left, res);
+    //     postorder(root->right, res);
+    //     res.push_back(root->val);
+    // }
     void postorder(TreeNode *root,vector<int> &res)
     {
         if(root==NULL)
             return;
-        postorder(root->left, res);
-        postorder(root->right, res);
-        res.push_back(root->val);
+        stack<TreeNode*> st1, st2;
+        st1.push(root);
+        while(!st1.empty())
+        {
+            root = st1.top();
+            st1.pop();
+            st2.push(root);
+            if(root->left)
+                st1.push(root->left);
+            if(root->right)
+                st1.push(root->right);
+            
+        }
+        while(!st2.empty())
+        {
+            res.push_back(st2.top()->val);
+            st2.pop();
+        }
     }
 };
