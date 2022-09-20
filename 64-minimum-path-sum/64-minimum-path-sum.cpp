0ad11f84dd8c03b5263008872388784baedd51f5
Prashant Maurya <62278497+prashant94157@users.noreply.github.com>
2022-09-20 10:35:22 +0530

    Time: 20 ms (32.63%), Space: 10 MB (66.21%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -20,27 +20,30 @@ class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid[0].size(), n = grid.size();
-        vector<vector<int>> res(n,vector<int> (m,0));
+        // vector<vector<int>> res(n,vector<int> (m,0));
+        vector<int> prev(m);
         // return minPath(grid,res);
         for(int i=0;i<n;i++)
         {
+            vector<int> curr(m);
             for(int j=0;j<m;j++)
             {
                 int ans = 1e9;
                 if(i==0 || j==0)
                 {
                     if(i==0 && j==0)
-                        res[i][j] = grid[i][j];
+                        curr[j] = grid[i][j];
                     else if(j==0)
-                        res[i][j] = grid[i][j] + res[i-1][j];
-                    else res[i][j] = grid[i][j] + res[i][j-1];
+                        curr[j] = grid[i][j] + prev[j];
+                    else curr[j] = grid[i][j] + curr[j-1];
                 }
                 else
-                res[i][j] = min(res[i-1][j]+grid[i][j], res[i][j-1]+grid[i][j]);
-                cout<<res[i][j]<<" ";
+                curr[j] = min(prev[j]+grid[i][j], curr[j-1]+grid[i][j]);
+                // cout<<res[i][j]<<" ";
             }
-            cout<<endl;
+            prev = curr;
+            // cout<<endl;
         }
-        return res[n-1][m-1];
+        return prev[m-1];
     }
 };
