6ad6efcd8a3c415b9ae4b2f71eed7ddb0e82a2c5
Prashant Maurya <62278497+prashant94157@users.noreply.github.com>
2022-09-20 11:02:42 +0530

    Time: 14 ms (29.42%), Space: 9 MB (52.45%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -10,29 +10,26 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n+1,vector<int> (n+1));
-        // return triangle[0][0] + recur(0,0,triangle,dp);    
-        dp[0][0] = triangle[0][0];
-        int res = dp[0][0];
+        // vector<vector<int>> dp(n+1,vector<int> (n+1));
+        // return triangle[0][0] + recur(0,0,triangle,dp); 
+        vector<int> prev(n);
+        prev[0] = triangle[0][0];
+        int res = prev[0];
         for(int i=1;i<n;i++)
         {
+            vector<int> curr(n);
             int currRes = INT_MAX;
             for(int j=0;j<=i;j++)
             {
                 if(i==j)
-                    dp[i][j] = dp[i-1][j-1] + triangle[i][j];
+                    curr[j] = prev[j-1] + triangle[i][j];
                 else if(j>0)
-                    dp[i][j] = min(dp[i-1][j-1] + triangle[i][j], triangle[i][j] + dp[i-1][j]);
-                else dp[i][j] = triangle[i][j] + dp[i-1][j];
-                currRes = min(dp[i][j], currRes);
+                    curr[j] = min(prev[j-1] + triangle[i][j], triangle[i][j] + prev[j]);
+                else curr[j] = triangle[i][j] + prev[j];
+                currRes = min(curr[j], currRes);
             }
+            prev = curr;
             res = currRes;
-        }
-        for(int i=1;i<n;i++)
-        {
-            for(int j=0;j<=i;j++)
-                cout<<dp[i][j]<<" ";
-            cout<<endl;
         }
         return res;
     }
