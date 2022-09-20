91d883f5d096619c4ea5335197e2aba17d8b6df4
Prashant Maurya <62278497+prashant94157@users.noreply.github.com>
2022-09-20 11:27:56 +0530

    Time: 29 ms (35.25%), Space: 10.3 MB (54.26%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,23 +1,40 @@
 class Solution {
-    int n;
-    int recur(vector<vector<int>>& matrix, int i,int j,vector<vector<int>> &dp)
-    {
-        if(i<0 || i>=n || j>=n || j<0)
-            return 1e9;
-        if(i==n-1)
-            return matrix[i][j];
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        int a = recur(matrix,i+1,j-1,dp), b = recur(matrix,i+1,j,dp), c = recur(matrix,i+1,j+1,dp);
-        return dp[i][j] = matrix[i][j] + min(min(a,b),c) ;
-    }
+    // int n;
+    // int recur(vector<vector<int>>& matrix, int i,int j,vector<vector<int>> &dp)
+    // {
+    //     if(i<0 || i>=n || j>=n || j<0)
+    //         return 1e9;
+    //     if(i==n-1)
+    //         return matrix[i][j];
+    //     if(dp[i][j] != -1)
+    //         return dp[i][j];
+    //     int a = recur(matrix,i+1,j-1,dp), b = recur(matrix,i+1,j,dp), c = recur(matrix,i+1,j+1,dp);
+    //     return dp[i][j] = matrix[i][j] + min(min(a,b),c) ;
+    // }
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        n = matrix.size();int res = INT_MAX;
-        vector<vector<int>> dp(n, vector<int> (n,-1));
+        int n = matrix.size();int res=0;
+        vector<vector<int>> dp(n, vector<int> (n,0));
         for(int i=0;i<n;i++)
         {
-            res = min(recur(matrix, 0,i,dp),res);
+            int currRes = INT_MAX;
+            for(int j=0;j<n;j++)
+            {
+                if(i==0)
+                    dp[i][j] = matrix[i][j];
+                else
+                {
+                    int a = 1e9, b = 1e9, c = 1e9;
+                    if(j>0)
+                        a = dp[i-1][j-1];
+                    b = dp[i-1][j];
+                    if(j<n-1)
+                        c = dp[i-1][j+1];
+                    dp[i][j] = matrix[i][j] + min(min(a,b),c) ;
+                }
+                currRes = min(dp[i][j], currRes);
+            }
+            res = currRes;
         }
         return res;
     }
