208c79b5711885374f13e66c671dc614b4732bd1
Prashant Maurya <62278497+prashant94157@users.noreply.github.com>
2022-09-20 11:00:02 +0530

    Time: 45 ms (5.60%), Space: 9.1 MB (32.63%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -10,7 +10,30 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
-        return triangle[0][0] + recur(0,0,triangle,dp);        
+        vector<vector<int>> dp(n+1,vector<int> (n+1));
+        // return triangle[0][0] + recur(0,0,triangle,dp);    
+        dp[0][0] = triangle[0][0];
+        int res = dp[0][0];
+        for(int i=1;i<n;i++)
+        {
+            int currRes = INT_MAX;
+            for(int j=0;j<=i;j++)
+            {
+                if(i==j)
+                    dp[i][j] = dp[i-1][j-1] + triangle[i][j];
+                else if(j>0)
+                    dp[i][j] = min(dp[i-1][j-1] + triangle[i][j], triangle[i][j] + dp[i-1][j]);
+                else dp[i][j] = triangle[i][j] + dp[i-1][j];
+                currRes = min(dp[i][j], currRes);
+            }
+            res = currRes;
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+                cout<<dp[i][j]<<" ";
+            cout<<endl;
+        }
+        return res;
     }
 };
