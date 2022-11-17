65dd245968dec11a08b5361b0f29276aaca4c8f2
Prashant Maurya <62278497+prashant94157@users.noreply.github.com>
2022-11-18 03:19:49 +0530

    Time: 14 ms (82.89%), Space: 10.9 MB (28.03%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string convert(string s, int n) {
+        if(n==1)
+            return s;
+        vector<string> arr(n);
+        int i=0,state=0;
+        for(auto a:s)
+        {
+            if(state==0 || i==0)
+            {
+                arr[i]+=a;
+                i++;
+                if(i==n)
+                {
+                    i-=2;
+                    state=1;
+                }
+            }
+            else
+            {
+                arr[i]+=a;
+                i--;
+                if(i<=0)
+                {
+                    i=0;
+                    state=0;
+                }
+            }
+        }
+        string str;
+        for(auto a:arr)
+        {
+            str+=a;
+        }
+        return str;
+    }
+};
